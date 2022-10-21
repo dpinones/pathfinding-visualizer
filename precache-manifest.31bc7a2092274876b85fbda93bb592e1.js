cea5eedd056e207aaef37881ef26830d92652152
dpinones <pinonesdamian@gmail.com>
2022-10-21 20:15:25 -0300

    add page static

---
+++ precache-manifest.31bc7a2092274876b85fbda93bb592e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13035f4411e994256e9bde9fd854f1a",
+    "url": "/pathfinding-visualizer/index.html"
+  },
+  {
+    "revision": "58d3cdbb6e5700fa30f7",
+    "url": "/pathfinding-visualizer/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "052edd8d369655afe0b6",
+    "url": "/pathfinding-visualizer/static/css/main.f93ca575.chunk.css"
+  },
+  {
+    "revision": "58d3cdbb6e5700fa30f7",
+    "url": "/pathfinding-visualizer/static/js/2.a20bc76f.chunk.js"
+  },
+  {
+    "revision": "cb685c785dda0c885e322af0f0892063",
+    "url": "/pathfinding-visualizer/static/js/2.a20bc76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052edd8d369655afe0b6",
+    "url": "/pathfinding-visualizer/static/js/main.c94326af.chunk.js"
+  },
+  {
+    "revision": "a2b7021554e2339a14f1",
+    "url": "/pathfinding-visualizer/static/js/runtime-main.b6ae0412.js"
+  },
+  {
+    "revision": "0057261d14be347efdfd2e72003943b2",
+    "url": "/pathfinding-visualizer/static/media/preview.0057261d.gif"
+  },
+  {
+    "revision": "18ae753445e74a1feb1ab69c07194c67",
+    "url": "/pathfinding-visualizer/static/media/resolved.18ae7534.gif"
+  }
+]);
